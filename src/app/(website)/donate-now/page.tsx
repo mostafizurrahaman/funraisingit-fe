fbdf548ef281472061ee4f0da37094e29e3ec33c
Rabeya Akter <rabeyariya251@gmail.com>
2026-07-06 11:22:35 +0600

    feat: add DonateNow component and DonateNowPage for donation functionality

---
+++ src/app/(website)/donate-now/page.tsx
@@ -0,0 +1,12 @@
+import DonateNow from '@/components/pages/DonateNow';
+import React from 'react';
+
+const DonateNowPage = () => {
+    return (
+        <div>
+            <DonateNow/>
+        </div>
+    );
+};
+
+export default DonateNowPage;
---
+++ src/components/pages/DonateNow.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DonateNow = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DonateNow;
