0287423c46f5ffb3e2488e92282855beb7b6b04a
Rabeya Akter <rabeyariya251@gmail.com>
2026-07-06 10:58:51 +0600

    Add DonationSuccess and orderSuccess components
    - Created DonationSuccess component with initial structure.
    - Created orderSuccess component with initial structure.

---
+++ src/components/pages/DonationSuccess.tsx
@@ -0,0 +1,12 @@
+
+import roket from "../../assets/roket.png";
+import glitter from "../../assets/glitter.png"
+const DonationSuccess = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DonationSuccess;
