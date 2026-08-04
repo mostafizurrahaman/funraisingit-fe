f139cb4a0349368b70fab7155d5348899c86e95a
RabeyaAkter78 <rabeyariya251@gmail.com>
2026-08-04 12:41:13 +0600

    implement crud on supporter management

---
+++ src/redux/features/SupportersApi/SupportersApi.ts
@@ -0,0 +1,61 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const SupportersApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // Create Supporter
+    createSupporter: builder.mutation({
+      query: (data) => ({
+        url: "/supporters",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Supporters"],
+    }),
+
+    // Get All Supporters
+    getAllSupporters: builder.query({
+      query: (params) => ({
+        url: "/supporters",
+        method: "GET",
+        params,
+      }),
+      providesTags: ["Supporters"],
+    }),
+
+    // Get Single Supporter
+    getSingleSupporter: builder.query({
+      query: (id) => ({
+        url: `/supporters/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["Supporters"],
+    }),
+
+    // Update Supporter
+    updateSupporter: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/supporters/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesTags: ["Supporters"],
+    }),
+
+    // Delete Supporter
+    deleteSupporter: builder.mutation({
+      query: (id) => ({
+        url: `/supporters/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Supporters"],
+    }),
+  }),
+});
+
+export const {
+  useCreateSupporterMutation,
+  useGetAllSupportersQuery,
+  useGetSingleSupporterQuery,
+  useUpdateSupporterMutation,
+  useDeleteSupporterMutation,
+} = SupportersApi;
